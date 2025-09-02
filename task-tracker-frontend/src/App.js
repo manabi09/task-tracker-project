a61bb14c1c7290d4bede881964c7895faf8d4caf
manabi09 <manabisadhukhan@gmail.com>
2025-09-02 19:36:15 +0530

    app.js modified

--- task-tracker-frontend/src/App.js
+++ task-tracker-frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { fetchTasks } from "./api";
+import UserForm from "./components/UserForm";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
-function App() {
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  async function loadTasks() {
+    const data = await fetchTasks();
+    setTasks(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "1rem" }}>
+      <h1>Task Tracker</h1>
+      <UserForm onUserAdded={() => {}} />
+      <TaskForm onTaskAdded={loadTasks} />
+      <TaskList tasks={tasks} />
     </div>
   );
 }
-
-export default App;
